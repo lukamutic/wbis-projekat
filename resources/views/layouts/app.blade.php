dee2d45586fbea1d01b8745226c4911a2b2be9a6
unknown <ml@sajtic.rs>
2023-02-01 17:21:05 +0100

    small addition in linking pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,11 @@
                             </li>
                         @endif
                     @else
+                    <li class="nav-item ">
+                              <a class="nav-link" href="{{ route ('new')}}">
+                    Create New Post
+                    </a>
+                    </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
